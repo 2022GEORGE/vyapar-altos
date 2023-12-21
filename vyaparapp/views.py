ff496749a4e872fc03a3305c8b265165f8480b2e
2022GEORGE <gm565361@gmail.com>
2023-12-21 22:31:32 +0530

    eight commit

--- vyaparapp/views.py
+++ vyaparapp/views.py
@@ -7626,52 +7626,36 @@ def send_sale_report_via_mail(request):
   if request.method == 'POST':
     emails=request.POST['email']
     mess=request.POST['message']
-    id=request.session.get('staff_id')
-    staff =  staff_details.objects.get(id=id)
-    sale = salesorder.objects.filter(comp=staff.company)
-    c=sale.count()
-    s=0
-    for i in sale:
-      s += float(i.grandtotal)
-    content={'sale':sale,'s':s,'c':c,'staff':staff}
-    template_path = 'company/share_salereport_mail.html'
-    template = get_template(template_path)
-    html  = template.render(content)
-    result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
-    pdf = result.getvalue()
-    email = EmailMessage(mess, from_email=settings.EMAIL_HOST_USER, to=[emails])
-    email.attach('salereport.pdf', pdf, "application/pdf")
-    email.send(fail_silently=False)
-    messages.success(request, 'Sale report file has been shared via email successfully..!')
-    return redirect('sales_report')
-  return redirect('sales_report')
+    uploaded_file = request.FILES.get('file')
+    if uploaded_file and uploaded_file.name.endswith('.pdf'):
+      subject = 'PDF Report'
+      message = mess
+      email = EmailMessage(subject, message, to=[emails])
+      email.attach(uploaded_file.name, uploaded_file.read(), 'application/pdf')
+      email.send()
+      messages.success(request, 'purchase report file has been shared via email successfully..!')
+      return redirect('sales_report')
+    else:
+      messages.success(request, '!Invalid pdf')
+      return redirect('sales_report')
+  return redirect('purchase_report')
 #------------------------------------------------------------------------------------
 def send_purchase_report_via_mail(request):
   if request.method == 'POST':
     emails=request.POST['email']
     mess=request.POST['message']
-    id=request.session.get('staff_id')
-    staff =  staff_details.objects.get(id=id)
-    purchase_data=PurchaseBill.objects.filter(company=staff.company)
-    debit_note=purchasedebit.objects.filter(company=staff.company)
-    paid = unpaid = total=0
-    content={
-      'bill':purchase_data,
-      'debit':debit_note,
-      'staff':staff,
-    }
-    template_path = 'company/share_purchase_report_mail.html'
-    template = get_template(template_path)
-    html  = template.render(content)
-    result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
-    pdf = result.getvalue()
-    email = EmailMessage(mess, from_email=settings.EMAIL_HOST_USER, to=[emails])
-    email.attach('purchasereport.pdf', pdf, "application/pdf")
-    email.send(fail_silently=False)
-    messages.success(request, 'purchase report file has been shared via email successfully..!')
-    return redirect('purchase_report')
+    uploaded_file = request.FILES.get('file')
+    if uploaded_file and uploaded_file.name.endswith('.pdf'):
+      subject = 'PDF Report'
+      message = mess
+      email = EmailMessage(subject, message, to=[emails])
+      email.attach(uploaded_file.name, uploaded_file.read(), 'application/pdf')
+      email.send()
+      messages.success(request, 'purchase report file has been shared via email successfully..!')
+      return redirect('purchase_report')
+    else:
+      messages.success(request, '!Invalid pdf')
+      return redirect('purchase_report')
   return redirect('purchase_report')
 #-------------------------------------------------------------------------------
 def day_book_report(request):
